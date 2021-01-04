0378a2075b6fbb6f3f9f895becf369db9df28515
Awais Anwar <74348183+awaisanwar12@users.noreply.github.com>
2021-01-04 20:48:50 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App"> 
-      Hello awais anwar you are naughty boy  vmvkvk
+      Hello awais anwar you are naughty boy  adn we are testing cicd
     </div>
   );
 }
